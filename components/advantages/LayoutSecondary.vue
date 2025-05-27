b1625762e3ecd2e2c0c0c469035914f729e26c30
Hassan Al Jammal <hassan-jammal@hotmail.com>
2025-05-27 15:04:45 +0300

    l

--- components/advantages/LayoutSecondary.vue
+++ components/advantages/LayoutSecondary.vue
@@ -1,7 +1,7 @@
 <template>
     <h2 class="text-xl lg:text-3xl font-semibold text-center">{{ productData.title }}</h2>
     <div class="grid sm:grid-cols-2 items-start gap-20 w-full lg:w-2/3 mx-auto mt-12">
-        <div v-for="(item, index) in productData.items" :key="index" class="flex items-center gap-4">
+        <div v-for="(item, index) in productData.items" :key="index" class="flex items-start gap-4">
             <NuxtImg class="flex-shrink-0" :src="`images/icons/${item.icon}.svg`" :alt="item.title" width="62" height="62" />
             <div>
                 <h2 class="text-lg lg:text-xl font-semibold">{{ item.title }}</h2>
